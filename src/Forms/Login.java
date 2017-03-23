415797e564d10a343ad9edeecaf028c9795a4a45
rt-chang <rchang21@gmail.com>
2017-03-23 16:54:44 -0700

    Starting forms. Issues divided.

---
+++ src/Forms/Login.java
@@ -0,0 +1,13 @@
+package Forms;
+
+import javax.swing.*;
+
+/**
+ * Created by Skyline on 2017-03-23.
+ */
+public class Login {
+    private JTextField username;
+    private JPanel panel1;
+    private JPasswordField passwordField;
+    private JButton loginButton;
+}
