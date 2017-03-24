d5eb3ae7296c155eb60ec732ab674a0940a3ec86
rt-chang <rchang21@gmail.com>
2017-03-23 17:08:45 -0700

    Starting forms. Issues divided.

--- src/Forms/Login.java
+++ src/Forms/Login.java
@@ -1,13 +1,24 @@
 package Forms;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
- * Created by Skyline on 2017-03-23.
+ * Created by Raymond on 2017-03-23.
  */
 public class Login {
     private JTextField username;
     private JPanel panel1;
     private JPasswordField passwordField;
     private JButton loginButton;
+
+    public Login() {
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
